f1abca6e6438a48df319892cdc2bcfb4d5bb5f8c
idarrr <baharmuzhaidar@gmail.com>
2017-11-26 22:00:02 +0700

    Add files via upload

---
+++ compiler.lua
@@ -0,0 +1,197 @@
+--[[
+NEXUS COMPILER
+
+WE DONT GIVE ALL ADMINS PERMISSION TO USE THIS.
+SO IF YOU WANT TO GIVE ACCESS TO SOMEONE TO USE THIS, PLEASE ADD THE ACCOUNT NAME TO ACCESS TABLE
+
+]]
+local access = {
+	idarrr = true,
+	Deihim007 = true
+}
+local QUEUE = {
+	compiling = false,
+	restarting = {},
+}
+
+
+function openGUI (thePlayer)
+
+	local account = getPlayerAccount (thePlayer)
+	if not account or isGuestAccount(account) then return end
+	local accname = getAccountName(account)
+	if not access[accname] then
+		outputDebugString("COMPILER: Access denied for account"..accname)
+		return
+	end
+
+	local res = getResources()
+	local data = {}
+	for i, v in pairs(res) do
+		local name = getResourceName(v)
+		local state = getResourceState(v)
+		local meta = xmlLoadFile(":".. name .."/meta.xml")
+		local child = xmlNodeGetChildren(meta)
+		local files = {}
+		for k, f in pairs(child) do
+			local name = xmlNodeGetName (f)
+			if name == "script" then
+				local attr = xmlNodeGetAttribute (f, "type")
+				if attr == "client" then
+					local path = xmlNodeGetAttribute (f, "src")
+					table.insert(files, path)
+				end
+			end
+		end
+		table.insert(data,{
+				name = name,
+				state = state,
+				files = files
+			})
+
+	end
+	triggerClientEvent(thePlayer, "compiler:open", thePlayer, data)
+end
+addCommandHandler("compile", openGUI)
+
+function compileResource (resname, restart)
+	local client = client
+	local account = getPlayerAccount (client)
+	if not account or isGuestAccount(account) then return end
+	local accname = getAccountName(account)
+	if not access[accname] then
+		outputDebugString("COMPILER: Access denied for account"..accname)
+		return
+	end
+
+	local resname = tostring(resname)
+	local meta = xmlLoadFile(":".. resname .."/meta.xml")
+	if not meta then
+		triggerClientEvent(client, "compiler:addLog", client, "Can't find meta.xml. Compile canceled.")
+		--outputDebugString("Can't find meta.xml. Compile canceled.")
+		return
+	end
+	triggerClientEvent(client, "compiler:addLog", client, "PLEASE DON'T DO ANY OPERATION UNTIL COMPILE IS DONE!\nChecking file...")
+	--outputDebugString("COMPILER: PLEASE DON'T DO ANY OPERATION UNTIL COMPILE IS DONE!")
+	outputDebugString("COMPILER: Checking file...")
+	local count = 0
+	done = 0
+	local child = xmlNodeGetChildren(meta)
+	for i, v in pairs(child) do
+		local name = xmlNodeGetName (v)
+		if name == "script" then
+			local attr = xmlNodeGetAttribute (v, "type")
+			if attr == "client" then
+				local path = xmlNodeGetAttribute (v, "src")
+				local realpath = ":".. resname .."/"..path..""
+				local luafile = path:sub(-3, -1) == "lua"
+				local luacfile = path:sub(-4, -1) == "luac"
+				if luafile then
+					local file = fileOpen(realpath)
+					local size = fileGetSize (file)
+					if file and (size ~= 0) then
+						local newpath = realpath.."c"
+						fetchRemote( "http://luac.mtasa.com/?compile=1&debug=0&obfuscate=2",  
+						function(data)  
+							local compiled = fileCreate ( newpath ) 
+							if compiled then
+
+								fileWrite(compiled, data)  
+								fileFlush(compiled) 
+								fileClose(compiled)
+								triggerClientEvent(client, "compiler:addLog", client, realpath.." compiled to ".. newpath)
+								--outputDebugString("COMPILER: "..realpath.." compiled to ".. newpath .."")
+								done = done + 1
+							end 
+						end, fileRead(file, fileGetSize ( file )) , true )--fileLoad(FROM) 
+						--https://luac.mtasa.com/api/ 
+						count = count + 1
+						fileClose(file)
+						local edit = xmlNodeSetAttribute (v, "src", path.."c")
+						xmlSaveFile(meta)
+					elseif size == 0 then
+						triggerClientEvent(client, "compiler:addLog", client, "Skipping file "..realpath..". Reason: file is empty.")
+					end
+				elseif luacfile then
+					local path = path:sub(0, -2)
+					local realpath = ":".. resname .."/"..path..""
+					if fileExists(realpath) then
+						local file = fileOpen(realpath)
+						if file then
+							local newpath = realpath.."c"
+							fetchRemote( "http://luac.mtasa.com/?compile=1&debug=0&obfuscate=2",  
+							function(data)  
+								local compiled = fileCreate ( newpath ) 
+								if compiled then 
+									fileWrite(compiled, data)  
+									fileFlush(compiled) 
+									fileClose(compiled)
+									triggerClientEvent(client, "compiler:addLog", client, ""..realpath.." replaced to ".. newpath .."")
+									--outputDebugString("COMPILER: "..realpath.." replaced to ".. newpath .."")
+									done = done + 1
+								end 
+							end, fileRead(file, fileGetSize ( file )) , true )--fileLoad(FROM) 
+							--https://luac.mtasa.com/api/ 
+							count = count + 1
+							fileClose(file)
+						end
+					end
+				end
+			end
+		end
+	end
+	xmlUnloadFile(meta)
+	if count == 0 then
+		outputDebugString("COMPILER: There is no client file to compile.")
+	else
+		checking = setTimer(
+			function ()
+				if count == done then
+					triggerClientEvent(client, "compiler:addLog", client, "COMPILER: Done! "..done.." file has been compiled.")
+					outputDebugString("COMPILER: Done! "..done.." file has been compiled.")
+					killTimer(checking)
+					if restart then
+						resResource(client, resname)
+					end
+				end
+			end
+		, 500, 0)
+		
+	end
+end
+addEvent("compiler:start", true)
+addEventHandler("compiler:start", root, compileResource)
+
+function resResource(thePlayer, resname)
+	if hasObjectPermissionTo (thePlayer, "command.restart", true) then
+		local res = getResourceFromName(resname)
+		if res then
+			restartResource(res)
+			QUEUE.restarting[""..resname..""] = thePlayer
+			triggerClientEvent(thePlayer, "compiler:addLog", thePlayer, "Restarting ("..resname..")...")
+		end
+	end
+end
+addEvent("compiler:restart", true)
+addEventHandler("compiler:restart", root, resResource)
+
+addEventHandler("onResourceStart", root,
+	function (res)
+		local name = getResourceName(res)
+		if name == "NGScompiler" then
+			if not hasObjectPermissionTo (res, "function.restartResource", true) then
+				outputDebugString("COMPILER: ACL function.restartResource request is needed.")
+			end
+			if not hasObjectPermissionTo (res, "function.fetchRemote", true) then
+				outputDebugString("COMPILER: ACL function.fetchRemote request is needed.")
+			end
+			if not hasObjectPermissionTo (res, "general.ModifyOtherObjects", true) then
+				outputDebugString("COMPILER: ACL general.ModifyOtherObjects request is needed.")
+			end
+		end
+		if QUEUE.restarting[""..name..""] then
+			local player = QUEUE.restarting[""..name..""]
+			triggerClientEvent(player, "compiler:addLog", player, "Resource ("..name..") restarted.")
+		end
+	end
+)
---
+++ main.lua
@@ -0,0 +1,106 @@
+local GUI = {}
+local sw, sh = guiGetScreenSize()
+local w, h = 500, 400
+local x, y = sw/2-w/2, sh/2-h/2
+local logs = "Nexus Client File Compiler\nSelect resource to compile."
+
+function openGUI (datas)
+	if GUI.window and isElement(GUI.window) then
+		return
+	end
+	GUI.window = guiCreateWindow(x, y, w, h, "Nexus Compiler", false)
+	guiWindowSetSizable (GUI.window, false)
+	GUI.version = guiCreateLabel(w-100, h-25, 100, 25, "By: Idarrr - V 1.0", false, GUI.window)
+	GUI.list = guiCreateGridList (5, 30, 220, h-40, false, GUI.window)
+	guiGridListAddColumn (GUI.list, "Resource", 0.6)
+	guiGridListAddColumn (GUI.list, "State", 0.25)
+	for i, v in pairs(datas) do
+		local row = guiGridListAddRow (GUI.list, v.name, v.state)
+		guiGridListSetItemText (GUI.list, row, 1, v.name, false, false )
+	end
+
+	GUI.log = guiCreateMemo(235, 30, w-235-5, 150, logs, false, GUI.window)
+	guiMemoSetReadOnly (GUI.log, true)
+	GUI.files = guiCreateGridList (235, 30+155, w-235-5, 150, false, GUI.window)
+	guiGridListAddColumn (GUI.files, "Client file", 0.8)
+	guiSetVisible(GUI.files, false)
+	GUI.button = {}
+	GUI.button[1] = guiCreateButton (235, 190+150, 80, 35, "Compile", false, GUI.window)
+	addEventHandler ( "onClientGUIClick", GUI.button[1],
+		function ()
+			local index = guiGridListGetSelectedItem(GUI.list)
+			local res = guiGridListGetItemText(GUI.list, index, 1)
+			triggerServerEvent("compiler:start", localPlayer, res)
+		end,
+	false )
+	GUI.button[2] = guiCreateButton (325, 190+150, 80, 35, "Compile and Restart", false, GUI.window)
+	addEventHandler ( "onClientGUIClick", GUI.button[2],
+		function ()
+			local index = guiGridListGetSelectedItem(GUI.list)
+			local res = guiGridListGetItemText(GUI.list, index, 1)
+			triggerServerEvent("compiler:start", localPlayer, res, true)
+		end,
+	false )
+	GUI.button[3] = guiCreateButton (415, 190+150, 80, 35, "Close", false, GUI.window)
+	addEventHandler ( "onClientGUIClick", GUI.button[3],
+		function ()
+			closeGUI ()
+		end,
+	false )
+	for i, v in pairs(GUI.button) do
+		--
+		guiSetVisible(v, false)
+	end
+
+	addEventHandler ( "onClientGUIClick", GUI.list, 
+		function ()
+			local index = guiGridListGetSelectedItem(GUI.list)
+			if index == -1 then
+				guiSetVisible(GUI.files, false)
+				for i, v in pairs(GUI.button) do
+					guiSetVisible(v, false)
+				end
+				return
+			end
+			local res = guiGridListGetItemText(GUI.list, index, 1)
+			for i, v in pairs(datas) do
+				if v.name == res then
+					addLog ("Resource: "..res..". Client file :"..#v["files"].." file(s).")
+					onResSelect (v["files"])
+					break
+				end
+			end
+			guiSetVisible(GUI.files, true)
+			for i, v in pairs(GUI.button) do
+				guiSetVisible(v, true)
+			end
+		end,
+	false )
+end
+addEvent("compiler:open", true)
+addEventHandler("compiler:open", root, openGUI)
+
+function onResSelect (files)
+	guiGridListClear (GUI["files"])
+	for i, v in pairs(files) do
+		guiGridListAddRow (GUI["files"], v)
+	end
+end
+
+function addLog (text)
+	local text = tostring(text)
+	logs = logs.."\n"..text
+	if GUI.log then
+		guiSetText(GUI.log, logs)
+		local ci = string.len(logs)
+		guiMemoSetCaretIndex(GUI.log,ci)
+	end
+end
+addEvent("compiler:addLog", true)
+addEventHandler("compiler:addLog", root, addLog)
+
+function closeGUI ()
+	if GUI.window and isElement(GUI.window) then
+		destroyElement(GUI.window)
+	end
+end
